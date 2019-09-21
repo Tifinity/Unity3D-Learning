04cfe938183552c387b36fb2765bc60c371bac3b
Tifinity <1139390530@qq.com>
2019-09-21 18:38:08 +0800

    3D牧师与魔鬼

---
+++ 项目三：牧师与魔鬼/scripts/Interfaces.cs
@@ -0,0 +1,20 @@
+﻿using ControllerApplication;
+
+namespace InterfaceApplication {
+    //场景接口
+    public interface ISceneController {
+        void LoadResources();
+    }
+
+    //用户接口，包含所有用户交互的事件
+    public interface IUserAction {
+        //移动船
+        void MoveBoat();
+        //移动角色
+        void MoveRole(RoleModel role);
+        //重新开始
+        void Restart();
+        //检查游戏状态
+        int Check();
+    }
+}
---
+++ 项目二：仿真太阳系/solar_beh.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class solar_beh : MonoBehaviour {
+    public Transform sun;
+    public Transform mercury;
+    public Transform venus;
+    public Transform earth;
+    public Transform mars;
+    public Transform jupiter;
+    public Transform saturn;
+    public Transform uranus;
+    public Transform neptune;
+    public Transform moonPoint;
+    public Transform moon;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        /*公转*/
+        mercury.RotateAround(this.transform.position, new Vector3(4, 10, 0), 47 * Time.deltaTime);
+        venus.RotateAround(this.transform.position, new Vector3(2,10, 0), 35 * Time.deltaTime);
+        earth.RotateAround(this.transform.position, new Vector3(1, -10, 0), -30 * Time.deltaTime);
+        moonPoint.RotateAround(this.transform.position, new Vector3(1, -10, 0), -30 * Time.deltaTime);
+        moon.RotateAround(moonPoint.position, new Vector3(1, 10, 0), 30 * Time.deltaTime);
+        mars.RotateAround(this.transform.position, new Vector3(2, 10, 0), 24 * Time.deltaTime);
+        jupiter.RotateAround(this.transform.position, new Vector3(2, -10, 0), -13 * Time.deltaTime);
+        saturn.RotateAround(this.transform.position, new Vector3(1, -10, 0), -9 * Time.deltaTime);
+        uranus.RotateAround(this.transform.position, new Vector3(2, 10, 0), 6 * Time.deltaTime);
+        neptune.RotateAround(this.transform.position, new Vector3(1, -10, 0), -5 * Time.deltaTime);
+        /*自传*/
+        earth.Rotate(Vector3.up * Time.deltaTime * 250);
+        moon.Rotate(Vector3.up * Time.deltaTime * 250);
+        mercury.Rotate(Vector3.up * Time.deltaTime * 300);
+        venus.Rotate(Vector3.up * Time.deltaTime * 280);
+        mars.Rotate(Vector3.up * Time.deltaTime * 220);
+        jupiter.Rotate(Vector3.up * Time.deltaTime * 180);
+        saturn.Rotate(Vector3.up * Time.deltaTime * 160);
+        uranus.Rotate(Vector3.up * Time.deltaTime * 150);
+        neptune.Rotate(Vector3.up * Time.deltaTime * 140);
+    }
+}
