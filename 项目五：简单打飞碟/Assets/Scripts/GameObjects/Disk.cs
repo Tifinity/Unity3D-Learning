7bd158673ce7c37c42fab5221e12bb180a5aabde
Tifinity <1139390530@qq.com>
2019-10-09 11:36:57 +0800

    disk

---
+++ 项目五：简单打飞碟/Assets/Scripts/GameObjects/Disk.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disk : MonoBehaviour {
+    public int type = 1;
+    public int score = 1;                               
+    public Color color = Color.white;                    
+}
